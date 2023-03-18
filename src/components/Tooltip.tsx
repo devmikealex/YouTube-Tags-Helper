8675f5b8c70e0c9f6f16b710a34dd6ec31f2c9ff
Mike Alex <devmikealex@gmail.com>
2023-03-18 03:34:23 +0300

    Reworked Tooltip to CSS hover

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,23 +1,10 @@
-import { useState } from 'react'
-
 function Tooltip({ text, children }: { text: string; children: React.ReactNode }) {
-    const [isVisible, setIsVisible] = useState(false)
     return (
-        <div
-            className=''
-            onMouseEnter={() => {
-                setIsVisible(true)
-            }}
-            onMouseLeave={() => {
-                setIsVisible(false)
-            }}
-        >
+        <div className='group'>
             {children}
-            {isVisible && (
-                <div className='fixed px-4 py-2 bottom-5 left-1/2 -translate-x-1/2 text-center text-sm z-50 border border-blue-500 bg-blue-200 text-indigo-900 rounded-lg shadow-lg'>
-                    {text}
-                </div>
-            )}
+            <div className='fixed opacity-0 invisible group-hover:visible group-hover:opacity-100 transition-opacity duration-500 px-4 py-2 bottom-5 left-1/2 -translate-x-1/2 text-center text-sm z-50 border border-blue-500 bg-blue-200 text-indigo-900 rounded-lg shadow-lg'>
+                {text}
+            </div>
         </div>
     )
 }
